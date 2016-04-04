4130a04a7bbc11e18f26253420d0dd9370f6b4bd
phanvannhien@gmail.com <phanvannhien@gmail.com>
2016-04-04 15:46:43 +0700

    Readme install & Seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,7 +11,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+    	Model::unguard();	
+        $this->call(UsersTableSeeder::class);
+        $this->call(CategoriesTableSeeder::class);
+        $this->call(ProductTableSeeder::class);
+        Model::reguard();
     }
 
     
