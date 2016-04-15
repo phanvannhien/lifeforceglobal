f23a5373d21a80f5da07874085f53a527df1ab4b
phanvannhien@gmail.com <phanvannhien@gmail.com>
2016-04-15 10:45:06 +0700

    Fix font

--- resources/views/front/includes/header.blade.php
+++ resources/views/front/includes/header.blade.php
@@ -16,7 +16,7 @@
 <link id="pagestyle" rel="stylesheet" type="text/css" href="{{ url('assets/css/skin-1.css') }}">
  
 <link href="{{ url('assets/css/style.css') }}" rel="stylesheet">
- 
+
  
 <!--[if lt IE 9]>
     <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
