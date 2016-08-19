7b1d89fb624860d63e940df7fb93013220712c4d
phanvannhien@gmail.com <phanvannhien@gmail.com>
2016-08-19 10:15:11 +0700

    Fisnished code

--- resources/views/front.bk/index.blade.php
+++
@@ -1,7 +0,0 @@
-@include ('front.includes.header')
-@include ('front.nav')
-<p>&nbsp;</p>
-<p>&nbsp;</p>
-{!! Site::renderCategoriesSections() !!}
-{!! Site::renderCategoriesProducts() !!}
-@include ('front.includes.footer')
